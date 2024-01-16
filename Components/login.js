9721d0ffc8281d91201b231dca06f993ff9744c5
Sai Raj Deep Thikkisetty <sairaj.thikkisetty@perficient.com>
2024-01-16 16:44:30 +0530

    Your commit message here

--- Components/login.js
+++ Components/login.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Pressable, Image, StyleSheet, Alert, ImageBackground } from 'react-native';
+import { View, Text, TextInput, Pressable, Image, StyleSheet, Alert, ImageBackground,TouchableWithoutFeedback } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Button, ActivityIndicator, Snackbar } from 'react-native-paper';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Import FontAwesome icons
 import { API_BASE_URL } from './config';
 
 const LoginScreen = () => {
@@ -12,6 +13,7 @@ const LoginScreen = () => {
   const [loading, setLoading] = useState(false);
   const [snackbarVisible, setSnackbarVisible] = useState(false);
   const navigation = useNavigation();
+  const [showPassword, setShowPassword] = useState(false);
 
   useEffect(() => {
     checkAuthentication();
@@ -74,59 +76,67 @@ const LoginScreen = () => {
   };
 
   return (
-    
-   
-      <View style={styles.container}>
-        <Image
-          style={styles.imagee}
-          source={{
-            uri: 'https://blogs.perficient.com/files/graham-PoP-blog-1-276x204.png',
-          }}
+    <View style={styles.container}>
+      <Image
+        style={styles.imagee}
+        source={{
+          uri: 'https://blogs.perficient.com/files/graham-PoP-blog-1-276x204.png',
+        }}
+      />
+
+      <Text style={styles.logo}>Login</Text>
+      <View style={styles.inputView}>
+        <Icon name="user" size={20} color="maroon" style={styles.icon} />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Username/Employee ID"
+          placeholderTextColor="maroon"
+          onChangeText={(text) => setIdentifier(text)}
         />
-
-        <Text style={styles.logo}>Login</Text>
-        <View style={styles.inputView}>
-          <TextInput
-            style={styles.inputText}
-            placeholder="Username/Employee ID"
-            placeholderTextColor="maroon"
-            onChangeText={(text) => setIdentifier(text)}
-          />
-        </View>
-        <View style={styles.inputView}>
-          <TextInput
-            style={styles.inputText}
-            placeholder="Password"
-            placeholderTextColor="maroon"
-            secureTextEntry
-            onChangeText={(text) => setPassword(text)}
-          />
-        </View>
-        <Pressable
-          style={styles.loginBtn}
-          onPress={handleLogin}
-          disabled={loading}
-        >
-          <Text style={styles.loginText}>LOGIN</Text>
-        </Pressable>
-
-        <Text style={styles.link} onPress={handleSignup}>
-          Don't have an account? Sign up here
-        </Text>
-
-        <Snackbar
-          visible={snackbarVisible}
-          onDismiss={onDismissSnackbar}
-          duration={3000}
-          action={{
-            label: 'OK',
-            onPress: onDismissSnackbar,
-          }}
-        >
-          Please enter both email/employee ID and password.
-        </Snackbar>
       </View>
-  
+      <View style={styles.inputView}>
+        <Icon name="lock" size={20} color="maroon" style={styles.icon} />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Password"
+          placeholderTextColor="maroon"
+          secureTextEntry={!showPassword}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableWithoutFeedback onPress={() => setShowPassword(!showPassword)}>
+          <Icon
+            name={showPassword ? 'eye-slash' : 'eye'}
+            size={20}
+            color="maroon"
+            style={styles.icon}
+          />
+        </TouchableWithoutFeedback>
+      </View>
+      <Pressable
+        style={styles.loginBtn}
+        onPress={handleLogin}
+        disabled={loading}
+      >
+        <Icon name="sign-in" size={20} color="white" style={styles.loginIcon} />
+        <Text style={styles.loginText}>LOGIN</Text>
+      </Pressable>
+
+      <Text style={styles.link} onPress={handleSignup}>
+        Don't have an account? Sign up here
+      </Text>
+
+      <Snackbar
+        visible={snackbarVisible}
+        onDismiss={onDismissSnackbar}
+        duration={3000}
+        action={{
+          label: 'OK',
+          onPress: onDismissSnackbar,
+        }}
+      >
+        Please enter both email/employee ID and password.
+      </Snackbar>
+    </View>
   );
 };
 
@@ -157,20 +167,27 @@ const styles = StyleSheet.create({
     elevation: 10,
   },
   inputView: {
+    flexDirection: 'row',
+    alignItems: 'center',
     width: '80%',
     backgroundColor: 'white',
     borderRadius: 25,
-    height: 50,
+    height: 55,
     marginBottom: 20,
     elevation: 10,
     justifyContent: 'center',
-    padding: 20,
+    padding: 15,
+  },
+  icon: {
+    marginRight: 10,
   },
   inputText: {
     height: 50,
     color: 'blue',
+    flex: 1,
   },
   loginBtn: {
+    flexDirection: 'row',
     width: '80%',
     backgroundColor: 'maroon',
     borderRadius: 25,
@@ -180,10 +197,12 @@ const styles = StyleSheet.create({
     marginTop: 10,
     elevation: 10,
   },
+  loginIcon: {
+    marginRight: 10,
+  },
   loginText: {
     color: 'white',
   },
-  
 });
 
 export default LoginScreen;
